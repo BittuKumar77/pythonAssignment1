5ba20e6e7f1f02c6997312a9fe7738da4ee24e42
BittuKumar77 <110903496+BittuKumar77@users.noreply.github.com>
2022-08-23 18:05:13 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,7 @@
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+
+a="Bittu"
+b=21
+c="BCA"
+d=1
+print(a,b,c,d,sep='\n')
---
+++ 9.py
@@ -0,0 +1,7 @@
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+a="Bittu"
+b=21
+c="BCA"
+d=1
+print(a,b,c,d)
